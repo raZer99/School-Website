45f2e1d5251e5c2a4460e3301a7a666fce7e6a4b
“Md <mdsameer8729@gmail.com>
2024-07-18 00:07:44 +0530

    Final Project

--- client/src/pages/Facilities.jsx
+++ client/src/pages/Facilities.jsx
@@ -1,130 +1,61 @@
 import styles from "../styles/facilities.module.css";
 import React from "react";
 
-import ControlledCarousel from "./ControlledCarousel";
+import ControlledCarousel from "../components/ControlledCarousel";
+import Footer from "../components/Footer";
+
+const Facility = ({ heading, description }) => {
+  return (
+    <div style={{ marginBottom: "28px" }}>
+      <h2 style={{ fontSize: "1.5rem", marginBottom: "10px" }}>{heading}</h2>
+      <p style={{ fontSize: "1rem" }}>{description}</p>
+    </div>
+  );
+};
 
 function Facilities() {
-    return (
-        <div style={{backgroundColor: "whitesmoke"}} >
-            <div className={styles.facilities}  >
-                <h1> Facilities </h1>
-                
-            </div>
-            <ControlledCarousel />
-            <div className={styles.contents} >
-                <h2>"Comprehensive School Facilities: Enriching Learning Environments"</h2> 
-                <ul>
-                    <li>
-                        <h3>Food</h3>
-                        <p>Mid-day meals are provided with proper hygiene</p>
-                    </li>
-
-                    <li>
-                        <h3>Swimming Pool</h3>
-                        <p>A well-constructed Swimming Pool with a trained guide</p>
-                    </li>
-
-                    <li>
-                        <h3>Badminton Court</h3>
-                        <p>An indoor Badminton Court for indoor activities and physical fitnees</p>
-                    </li>
-
-                    <li>
-                        <h3>Auditorium</h3>
-                        <p>A Large Auditorium for daily prayers and annual functions</p>
-                    </li>
-
-                    <li>
-                        <h3>Labs</h3>
-                        <p>Embarking students' practical knowlege with computer, physics and chemistry labs</p>
-                    </li>
-
-                    <li>
-                        <h3>Multi-purpose Ground</h3>
-                        <p>Ground for outdoor games like football, cricket etc</p>
-                    </li>
-
-                    <li>
-                        <h3>Park</h3>
-                        <p>Greenery for students' refreshment</p>
-                    </li>
-
-                    <li>
-                        <h3>Smart Class</h3>
-                        <p>Smart class for smart students</p>
-                    </li>
-
-                    <li>
-                        <h3>Infrastuctures</h3>
-                        <p>Well constructed buildings with large corridors and solarpanels </p>
-                    </li>
-                   
-                </ul>
-            </div>
+  return (
+    <div className={styles["facilities"]}>
+      <div className={styles["facilities-container"]}>
+        <div className={styles["carousel"]}>
+          <ControlledCarousel />
         </div>
-        
-    );
+        <div className={styles["content"]}>
+          <div className="first-part">
+            <Facility
+              heading="Auditorium for Cultural Events"
+              description="Our auditorium serves as a hub for cultural events, performances, and gatherings. With state-of-the-art audio-visual facilities, it provides an ideal platform for students to showcase their talents in the performing arts and for the community to come together."
+            />
+            <Facility
+              heading="Science Laboratories"
+              description=" Explore the wonders of science in our advanced laboratories. Equipped with cutting-edge apparatus, our science labs provide students with hands-on experiences, fostering a love for scientific inquiry and discovery."
+            />
+          </div>
+          <div className="second-part">
+            <Facility
+              heading="Library and Study Halls"
+              description="Our extensive library and quiet study halls offer a conducive atmosphere for academic pursuits. Students have access to a wide range of books, journals, and online resources to support their learning and research endeavors."
+            />
+            <Facility
+              heading="Computer and Science Laboratories"
+              description="Explore the wonders of computers and science in our advanced laboratories. Equipped with cutting-edge apparatus, our science labs provide students with hands-on experiences, fostering a love for scientific inquiry and discovery."
+            />
+          </div>
+          <div className="second-part">
+            <Facility
+              heading="Digital Learning Center"
+              description="Embrace the future of education in our digital learning center. Equipped with the latest technology, students can engage in interactive and immersive learning experiences, enhancing their skills and knowledge in various subjects."
+            />
+            <Facility
+              heading="Multipurpose Sports Field and Indoor Badminton Court"
+              description="Our multipurpose sports field is designed to accommodate a variety of sports activities, from football and cricket to athletics. Our indoor badminton court is equipped with professional-grade flooring and lighting, providing the perfect environment for badminton enthusiasts."
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Facilities;
-
-    // {/* <div className="contents">
-        //         <div className="contents-items">
-        //             <h2>Food</h2>
-        //             <img src={food} alt="Food img" height={400} width={800} />
-        //             <p>Mid-day meals are provided with proper hygiene</p>
-        //         </div>
-
-        //         <div className="contents-items">
-        //             <h2>Swimming Pool</h2>
-        //             <img src={pool} alt="Pool img" height={400} width={800} />
-        //             <p>A well-constructed Swimming Pool with a trained guide</p>
-        //         </div>
-
-        //         <div className="contents-items">
-        //             <h2>Indoor Badminton Court</h2>
-        //             <img src={court} alt="Court img" height={400} width={800} />
-        //             <p>An indoor Badminton Court for indoor activities and physical fitnees</p>
-        //         </div>
-
-        //         <div className="contents-items">
-        //             <h2>Auditorium</h2>
-        //             <img src={auditorium} alt="Auditorium img" height={400} width={800} />
-        //             <p>A Large Auditorium for daily prayers and annual functions</p>
-        //         </div>
-
-        //         <div className="contents-items">
-        //             <h2>Computer Lab</h2>
-        //             <img src={lab} alt="Lab img" height={400} width={800} />
-        //             <p>Computer Labs for teaching students the uses and functions of computers</p>
-        //         </div>
-
-        //         <div className="contents-items">
-        //             <h2>Ground</h2>
-        //             <img src={ground} alt="Ground img" height={400} width={800} />
-        //             <p>Ground for outdoor games like football, cricket etc</p>
-        //         </div>
-
-        //         <div className="contents-items">
-        //             <h2>Park</h2>
-        //             <img src={park} alt="Park img" height={400} width={800} />
-        //             <p>Park for students' refreshment</p>
-        //         </div>
-
-        //         <div className="contents-items">
-        //             <h2>Smart Class</h2>
-        //             <img src={smartclass1} alt="Smart-class img" height={400} width={400} />
-        //             <img src={smartclass2} alt="Smart-class img" height={400} width={400} />
-        //             <p>Smart class for smart students</p>
-        //         </div>
-
-        //         <div className="contents-items">
-        //             <h2>Infrastructers</h2>
-        //             <img src={corridor} alt="Others img" height={400} width={266.66} />
-        //             <img src={building} alt="Others img" height={400} width={266.67} />
-        //             <img src={solarpanel} alt="Others img" height={400} width={266.66} />
-
-        //         </div>
-
-        //     </div> */}
-        // </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,36 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import HomeContent from "../components/HomeContent";
+import styles from "../styles/homepage.module.css";
+import Footer from "../components/Footer";
+import ReactPlayer from "react-player";
 
 function Home() {
   return (
-    <div>
-      <Navbar />
-      <div className="home-hero">
-        <img src="../assets/home-background.jpg" alt="" />
-        <h1>The Government High School, Rajanakunte</h1>
+    <div className={styles.homeMain}>
+      {/* <div className={styles["home-hero"]}>
+        <img
+          className={styles.homeImg}
+          src={require("../assets/hero-bg.jpg")}
+          alt=""
+        />
+        <h1 className={styles.homeH1}>GOVERNMENT INSTITUTIONS RAJANUKUNTE</h1>
+      </div> */}
+      <div className={styles["video-background"]}>
+        <ReactPlayer
+          className={styles["video"]}
+          url={require("../assets/videos/main-ideo.mp4")}
+          playing
+          loop
+          muted
+          width="100%"
+          height="50%"
+        />
+        <div className={styles["text-overlay"]}>
+          <h1>ಸರ್ಕಾರಿ ಸಂಸ್ಥೆಗಳು ರಾಜಾನುಕುಂಟೆ</h1>
+        </div>
       </div>
+      <HomeContent />
+      <Footer />
     </div>
   );
 }
